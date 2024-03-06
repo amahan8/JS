5d7ee4a2757cbe57186fa20856da9b3605a7f412
amahan8 <115523994+amahan8@users.noreply.github.com>
2024-03-06 02:55:07 +0000

    Conversion of datatypes

---
+++ 01_basics/Conversion_of_datatypes.js
@@ -0,0 +1,22 @@
+// let Numberage=33;
+
+// let Stringage=String(33);
+// console.log(Stringage);
+// console.log(typeof Stringage);
+
+// let StringNumber="343dfvf";
+
+// let Numberage= Number(StringNumber);
+// console.log(Numberage);
+// console.log(typeof Numberage);
+
+//"33abc" => NAN
+//"33" => 33
+//null => 0
+//true => 1
+//false=> 0
+
+let Numbervalue=1;
+let Booleanvalue=Boolean(Numbervalue);
+console.log(Booleanvalue);
+
