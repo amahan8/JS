e344cd065337dbe34667ee3f1e4f721adc8ac092
amahan8 <115523994+amahan8@users.noreply.github.com>
2024-03-07 02:50:20 +0000

    Number

---
+++ 01_basics/Number.js
@@ -0,0 +1,14 @@
+const score=40000000;
+console.log(score);
+
+const nu= new Number(300);
+console.log(nu);
+
+console.log(nu.toFixed(2));
+
+console.log(score.toLocaleString());
+
+const otherNumber=52365.08809;
+
+console.log(otherNumber.toPrecision(2));
+
