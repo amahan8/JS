f12d605559a1deecbe5aadf3c4c98a6023e98766
amahan8 <115523994+amahan8@users.noreply.github.com>
2024-03-17 18:27:40 +0000

    Whiledowhile

---
+++ 01_basics/Whiledowhile.js
@@ -0,0 +1,18 @@
+let i=0;
+while(i<=10){
+    console.log(`${i} is the value`);
+    i=i+2;
+
+}
+const myarr=["first","second","third"];
+let arr=0;
+while(arr<myarr.length){
+    console.log(myarr[arr]);
+    arr++;
+}
+
+let score=1;
+do{
+    console.log(`${score} is the value of score`);
+    score++;
+}while(score<=10)
