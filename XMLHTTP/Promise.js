54ef15bc71848d9a3b2f4ce2a7a44d8d88a4e913
amahan8 <115523994+amahan8@users.noreply.github.com>
2024-04-17 03:16:08 +0000

    Promsiefour

--- XMLHTTP/Promise.js
+++ XMLHTTP/Promise.js
@@ -2,9 +2,54 @@ const promiseOne=new Promise(function(resolve,reject){
      setTimeout(function(){
         console.log("Async task");
         resolve();
-     })
+     },1000)
 });
 
 promiseOne.then(function(){
     console.log("Promise Consumed");
+})
+
+// Promise 2
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log(" Asyn task started")
+        resolve()
+    },1000)
+    
+}).then(function(){
+    console.log(" Async resolved");
+})
+
+// Promise 3
+
+const promiseThree=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:"chai",email:" char@get.com"})
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+
+const promiseFour=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true;
+        if(!error){
+            resolve({user:"hitesh",email:"hitesh@net"})
+        }
+        else{
+            reject('Rejection')
+        }
+    },1000)
+})
+
+promiseFour.then(function(user){
+    console.log(user);
+    return user.user;
+}).then(function(username){
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
 })
