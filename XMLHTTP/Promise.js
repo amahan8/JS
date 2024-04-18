c8c45da549fc7eb3924339d8b4e9342634cb8d76
amahan8 <115523994+amahan8@users.noreply.github.com>
2024-04-18 02:48:20 +0000

    Promise

--- XMLHTTP/Promise.js
+++ XMLHTTP/Promise.js
@@ -52,4 +52,53 @@ promiseFour.then(function(user){
     console.log(username);
 }).catch(function(error){
     console.log(error);
+}).finally(()=>console.log("Promise resolved or rejected"));
+
+// ******async await*********
+
+const promiseFive=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true;
+        if(!error){
+            resolve({user: "Javsacript", password:123})
+        }
+        else{
+            reject("JS has an error");
+        }
+    },1000)
 })
+
+async function consumePromiseFive(){
+    try{
+    const response=await promiseFive;
+    console.log(response);
+}catch(error){
+    console.log(error);
+}
+}
+
+consumePromiseFive();
+
+// async with fetch
+async function getAllUsers(){
+    try{
+    const response=await fetch("https://jsonplaceholder.typicode.com/users");
+     const data= await response.json();
+    console.log(data);
+}catch(error){
+    console.log("E:" ,error);
+}
+}
+
+getAllUsers();
+
+//**** fetch*****
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(function(response){
+ return response.json;
+}).then(function(data){
+    console.log(data);
+}).catch(function(error){
+    console.log(error);
+});
